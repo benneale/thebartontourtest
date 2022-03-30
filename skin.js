8102f0a8d168a1c99839d467b8a48b1c07078bf0
Ben Neale <ben.neale@hotmail.com>
2022-03-30 12:10:20 +1030

    compressing

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.1.14/18105
 // Filename: neto_test.ggsk
-// Generated 2022-03-30T12:02:10
+// Generated 2022-03-30T12:05:01
 
 function pano2vrSkin(player,base) {
 	player.addVariable('vis_controls', 2, true);
