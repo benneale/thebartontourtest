15769f4277033351504df5f7e4484bb46841459c
Ben Neale <ben.neale@hotmail.com>
2022-03-30 16:40:08 +1030

    key

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.1.14/18105
 // Filename: neto_nopic.ggsk
-// Generated 2022-03-30T15:19:59
+// Generated 2022-03-30T16:38:50
 
 function pano2vrSkin(player,base) {
 	player.addVariable('vis_controls', 2, true);
